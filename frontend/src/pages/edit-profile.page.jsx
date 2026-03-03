506ef9e12552b92fb50b3317d5295a6470c6d16b
harishsehlangia <harish82817@gmail.com>
2026-03-03 22:22:31 +0530

    feat(settings): add edit profile API integration

--- frontend/src/pages/edit-profile.page.jsx
+++ frontend/src/pages/edit-profile.page.jsx
@@ -1,21 +1,27 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { UserContext } from "../App";
 import axios from "axios";
 import { profileDataStructure } from "./profile.page";
 import AnimationWrapper from "../common/page-animation";
 import Loader from "../components/loader.component";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import InputBox from "../components/input.component";
+import { uploadImage } from "../common/aws";
+import { storeInSession } from "../common/session";
 
 const EditProfile = () => {
 
-    let { userAuth, userAuth: { access_token } } = useContext(UserContext);
+    let { userAuth, userAuth: { access_token }, setUserAuth } = useContext(UserContext);
 
     let bioLimit = 150;
+
+    let profileImgEle = useRef();
+    let editProfileForm = useRef();
     
     const [ profile, setProfile ] = useState(profileDataStructure);
     const [ loading, setLoading ] = useState(true);
     const [ charactersLeft, setCharactersLeft ] = useState(bioLimit);
+    const [ updatedProfileImg, setUpdatedProfileImg ] = useState(null);
 
     let { personal_info: { fullname, username: profile_username, profile_img, email, bio }, social_links } = profile;
 
@@ -38,11 +44,120 @@ const EditProfile = () => {
         setCharactersLeft(bioLimit - e.target.value.length)
     }
 
+    const handleImagePreview = (e) => {
+
+        let img = e.target.files[0];
+
+        profileImgEle.current.src = URL.createObjectURL(img);
+
+        setUpdatedProfileImg(img);
+
+    }
+
+    const handleImageUpload = (e) => {
+
+        e.preventDefault();
+
+        if(updatedProfileImg){
+            
+            let loadingToast = toast.loading("Uploading....");
+            e.target.setAttribute("disabled", true);
+
+            uploadImage(updatedProfileImg)
+            .then(url => {
+                
+                if(url){
+                    axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/update-profile-img", { url }, {
+                        headers: {
+                            'Authorization': `Bearer ${access_token}`
+                        }
+                    })
+                    .then(({ data }) => {
+                        let newUserAuth = { ...userAuth, profile_img: data.profile_img }
+
+                        storeInSession("user", JSON.stringify(newUserAuth));
+                        setUserAuth(newUserAuth);
+
+                        setUpdatedProfileImg(null);
+                        toast.dismiss(loadingToast);
+                        e.target.removeAttribute("disabled");
+                        toast.success("Uploaded 👍");
+                    })
+                    .catch(({response}) => {
+                        toast.dismiss(loadingToast);
+                        e.target.removeAttribute("disabled");
+                        toast.error("response.data.error");
+                    })
+                }
+
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+        }
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let form = new FormData(editProfileForm.current);
+        let formData = { };
+
+        for(let [key, value] of form.entries()){
+            formData[key] = value;
+        }
+
+        let { username, bio, youtube, facebook, twitter, github, instagram, website } = formData;
+
+        if(username.length < 3){
+            return toast.error("Username should be at least 3 letters long");
+        }
+        if(bio.length > bioLimit){
+            return toast.error(`Bio should not be more than ${bioLimit} characters`)
+        }
+
+        let loadingToast = toast.loading("Updating.....");
+        e.target.setAttribute("disabled", true);
+
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/update-profile", {
+            username, bio,
+            social_links: { youtube, facebook, twitter, github, instagram, website }
+        }, {
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            }
+        })
+        .then(({ data }) => {
+
+            if(userAuth.username != data.username){
+
+                let newUserAuth = { ...userAuth, username: data.username };
+
+                storeInSession("user", JSON.stringify(newUserAuth));
+                setUserAuth(newUserAuth);
+
+            }
+
+            toast.dismiss(loadingToast);
+            e.target.removeAttribute("disabled");
+            toast.success("Profile Updated");
+
+        })
+        .catch(({ response }) => {
+            toast.dismiss(loadingToast);
+            e.target.removeAttribute("disabled");
+            toast.error(response.data.error);
+        })
+
+    }
+
     return (
         <AnimationWrapper>
             {
                 loading ? <Loader /> : 
-                <form>
+                <form ref={editProfileForm}>
                     <Toaster />
 
                     <h1 className="max-md:hidden">Edit Profile</h1>
@@ -55,11 +170,11 @@ const EditProfile = () => {
                                 <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center text-white bg-black/50 opacity-0 hover:opacity-100 cursor-pointer">
                                     Upload Image
                                 </div>
-                                <img src={profile_img} />
+                                <img ref={profileImgEle} src={profile_img} />
                             </label>
-                            <input type="file" id="uploadImg" accept=".jpeg, .png, .jpg" hidden />
+                            <input type="file" id="uploadImg" accept=".jpeg, .png, .jpg" hidden onChange={handleImagePreview} />
 
-                            <button className="btn-light mt-5 max-lg:center lg:w-full px-10">Upload</button>
+                            <button className="btn-light mt-5 max-lg:center lg:w-full px-10" onClick={handleImageUpload}>Upload</button>
                         </div>
 
                         <div className="w-full">
@@ -100,7 +215,7 @@ const EditProfile = () => {
 
                             </div>
 
-                            <button className="btn-dark w-auto px-10" type="submit">Update</button>
+                            <button className="btn-dark w-auto px-10" type="submit" onClick={handleSubmit}>Update</button>
 
                         </div>
                     </div>
--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ import User from './Schema/User.js';
 import Blog from './Schema/Blog.js';
 import Notification from './Schema/Notification.js';
 import Comment from './Schema/Comment.js';
+import { json } from "node:stream/consumers";
 
 const server = express();
 let PORT = process.env.PORT || 3000;
@@ -425,6 +426,71 @@ server.post("/get-profile", (req, res) => {
         console.log(err);
         return res.status(500).json({ error: err.message })
     })
+})
+
+server.post("/update-profile-img", verifyJWT, (req, res) => {
+
+    let {url} = req.body;
+
+    User.findOneAndUpdate({ _id: req.user }, { "personal_info.profile_img": url })
+    .then(() => {
+        return res.status(200).json({ profile_img: url })
+    })
+    .catch(err => {
+        return res.status(500).json({ error: err.message })
+    })
+
+})
+
+server.post("/update-profile", verifyJWT, (req, res) => {
+
+    let { username, bio, social_links } = req.body;
+    let bioLimit = 150;
+
+    if(username.length < 3){
+        return res.status(403).json({ error: "Username should be at least 3 letters long" })
+    }
+    if(bio.length > bioLimit){
+        return res.status(403).json({ error: `Bio should not be more than ${bioLimit} characters` })
+    }
+
+    let socialLinksArr = Object.keys(social_links);
+
+    try {
+
+        for(let i=0; i < socialLinksArr.length; i++){
+            if(social_links[socialLinksArr[i]].length){
+                let hostname = new URL(social_links[socialLinksArr[i]]).hostname;
+
+                if(!hostname.includes(`${socialLinksArr[i]}.com`) && socialLinksArr[i] != 'website'){
+                    return res.status(403).json({ error: `${socialLinksArr[i]} link is invalid. You must enter a full link` })
+                }
+            }
+        }
+
+    } catch (err) {
+        return res.status(500).json({ error: "You must provide full social links with http(s) included" })
+    }
+
+    let updateObj = {
+        "personal_info.username": username,
+        "personal_info.bio": bio,
+        social_links
+    }
+
+    User.findOneAndUpdate({ _id: req.user }, updateObj, {
+        runValidators: true
+    })
+    .then(() => {
+        return res.status(200).json({ username })
+    })
+    .catch(err => {
+        if(err.code == 11000){
+            return res.status(409).json({ error: "username is already taken" })
+        }
+        return res.status(500).json({ error: err.message })
+    })
+
 })
 
 // create blog
